f96af7c8e511174019505f39b95890347627f48c
mabel <mabeloshua88@gmail.com>
2024-05-16 12:24:33 -0400

    my second commit

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "name" {
   ami             = data.aws_ami.linux2.id
   instance_type   = "t2.medium"
   security_groups = [aws_security_group.jenkins.name]
-  key_name        = "kingsleykeypair"
+  key_name        = "testing-kp"
 
   tags = {
     Name = "jenkins-zikora"
--- provider.tf
+++ provider.tf
@@ -13,7 +13,7 @@ terraform {
 provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
-  region  = "us-east-2"
+  region  = "us-east-1"
   # profile = "default"
 }
 
